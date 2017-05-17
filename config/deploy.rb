5a1b24bda83bb67f88bb6ae3d22be0e6580707c5
dalezhang <17652709@qq.com>
2017-05-17 14:46:23 +0800

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :branch, 'deploy'
 
 # shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 # set :shared_dirs, fetch(:shared_dirs, []).push('somedir')
-# set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml', 'config/puma.rb')
 
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
@@ -35,24 +35,30 @@ task :environment do
 
   # For those using RVM, use this to load an RVM version@gemset.
   puts "rvm:use"
-  invoke :'rvm:use', 'ruby-2.3.4@default'
+  invoke :'rvm:use', 'ruby-2.3.4'
 end
 
 # Put any custom commands you need to run at setup
 # All paths in `shared_dirs` and `shared_paths` will be created on their own.
 task :setup do
   # command %{rbenv install 2.3.0}
+  in_path './prgs' do
+    command %{pwd}
+    command %{cp -R shared/config #{fetch(:deploy_to)}/shared}
+    command %{sed -i 's/SECRET/#{SecureRandom.hex(64)}/g' #{fetch(:deploy_to)}/shared/config/secrets.yml}
+    command %{sed -i '1s/APP_NAME/#{app_name}/1' #{fetch(:deploy_to)}/shared/config/puma.rb}
+  end
 end
 
 desc "Deploys the current version to the server."
 task :deploy do
   # uncomment this line to make sure you pushed your local branch to the remote origin
   # invoke :'git:ensure_pushed'
-  
   deploy do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
     invoke :'git:clone'
+		invoke :clean_shared_files
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'deploy:cleanup'
@@ -66,7 +72,7 @@ task :deploy do
         command %{ ruby -v }
         command %{ ls /home/rb/prgs/merchant_info/current}
         command %{ ls /home/rb/tmp/pids/}
-        command %{ eye start merchant_info}
+        command %{ eye restart merchant_info}
       end
     end
   end
