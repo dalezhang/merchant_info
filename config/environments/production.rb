2bb4f10f77ff5f0d0086abcbf6997a89ff9af1ee
dalezhang <17652709@qq.com>
2017-05-18 17:07:14 +0800

    fixbug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,8 +29,6 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
-  config.assets.css_compressor = :yui
-  config.assets.js_compressor = :uglifier
   config.public_file_server.enabled = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
