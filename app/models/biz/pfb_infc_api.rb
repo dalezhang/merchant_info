45d7451c20dc297721a999bd0a0f1966621cbd9e
dalezhang <17652709@qq.com>
2017-06-02 17:03:40 +0800

    fixed

--- app/models/biz/pfb_infc_api.rb
+++ app/models/biz/pfb_infc_api.rb
@@ -78,14 +78,12 @@ module Biz
     def get_mab(js)
       mab = []
       js.keys.sort_by {|x| x.downcase}.each do |k|
-        mab << "#{k}=#{js[k]}" if k != :mac && k != :sign && js[k]
+        mab << "#{k}=#{js[k]}" if k != :mac && k != :sign && js[k].present?
       end
       mab.join('&')
     end
 
     def get_mac(js, key)
-      log_error @merchant, 'js', js
-      log_error @merchant, 'js', key
       Digest::MD5.hexdigest(get_mab(js).to_s + key.to_s).upcase
     end
   end
