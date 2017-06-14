029c7e89e64ebff20e6b9bd85be514c08865df22
dalezhang <17652709@qq.com>
2017-06-14 14:59:18 +0800

    123

--- app/controllers/api/merchants_controller.rb
+++ app/controllers/api/merchants_controller.rb
@@ -99,10 +99,12 @@ class Api::MerchantsController < ActionController::API
     md5(get_mab(js) + "&key=#{key}").upcase
   end
   def get_user
+    puts "params #{params}"
     unless params[:partner_id].present?
       raise 'partner_id为空'
     end
     @user = User.find_by(partner_id: params[:partner_id])
+    puts "@user #{@user}"
     unless @user.present?
       raise '找不到代理商信息，partner_id无效。'
     end
