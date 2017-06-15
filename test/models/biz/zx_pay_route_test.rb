3cbb082a66eac0f6e42b0da00c6ca9b64123240d
dalezhang <17652709@qq.com>
2017-06-15 11:41:35 +0800

    backup

--- test/models/biz/zx_pay_route_test.rb
+++ test/models/biz/zx_pay_route_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class ZxPayRouteTest < ActiveSupport::TestCase
   test "real test" do
-    #clean_database
-    #user = User.create(email: 'test@mail.com')
-    #merchant = create(:merchant, user: user, out_mch_id: '123')
+    clean_database
+    user = User.create(email: 'test@mail.com')
+    merchant = create(:merchant, user: user, out_mch_id: '123')
 
-    #biz = Biz::PayRoute::ZxAlipayPayRoute.new merchant
-    #biz.create_route
-    #assert merchant.merchant_id
+    biz = Biz::PayRoute::BjrcbPayRoute.new merchant
+    biz.send_wechat_offline
+    assert merchant.merchant_id
   end
   def clean_database
     User.destroy_all
