0b0047467f8a40827d397d5ea834e268a99b1254
dalezhang <17652709@qq.com>
2017-06-26 18:44:37 +0800

    123

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -3,6 +3,7 @@
 class TestController < AdminController
   include Logging
   def wechat_cert
+    key = '7H5sFkI3cJ32yD9bV6G224gH5hAZPgZK'
 
     if params[:commit] == '创建'
       url = 'https://api.mch.weixin.qq.com/secapi/mch/addsubdevconfig'
@@ -14,7 +15,7 @@ class TestController < AdminController
         sub_appid: params[:item][:sub_appid], # 子商户SubAPPID
         subscribe_appid: params[:item][:subscribe_appid], # 微信分配的服务商公众号或 APP 账号 ID；如为空，则值传NULL（字母大写小写均可）
       }
-      js[:sign] = get_mac js
+      js[:sign] = get_mac js, key
       xml = js.to_xml(root: 'xml', skip_instruct: true, dasherize: false)
       @request = xml
       @response = Biz::WechatCert.post(url, body: xml, verify: false)
@@ -25,7 +26,7 @@ class TestController < AdminController
         mch_id: params[:item][:mch_id], # 商户号
         sub_mch_id: params[:item][:sub_mch_id], #子商户号
       }
-      js[:sign] = get_mac js
+      js[:sign] = get_mac js, key
       xml = js.to_xml(root: 'xml', skip_instruct: true, dasherize: false)
       @request = xml
       @response = Biz::WechatCert.post(url, body: xml, verify: false)
@@ -47,7 +48,8 @@ class TestController < AdminController
   def md5(str)
     Digest::MD5.hexdigest(str)
   end
-  def get_mac(js)
-    md5(get_mab(js)).upcase
+  def get_mac(js, key)
+    md5(get_mab(js) + "&key=#{key}").upcase
   end
+
 end
