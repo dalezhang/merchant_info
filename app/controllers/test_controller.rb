2df50f08f4c63bbaeed5a51acbf4c9cfd76bd276
dalezhang <17652709@qq.com>
2017-06-28 10:18:06 +0800

    123

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -7,14 +7,15 @@ class TestController < AdminController
 
     if params[:commit] == '创建'
       url = 'https://api.mch.weixin.qq.com/secapi/mch/addsubdevconfig'
-      js = {
+      js = {}
+      {
         appid: params[:item][:appid], # 微信分配的公众账号 ID
         mch_id: params[:item][:mch_id], # 商户号
         sub_mch_id: params[:item][:sub_mch_id], #子商户号
         jsapi_path:  params[:item][:jsapi_path], # 子商户公众账号 JSAPI 支付授权目录子商户
         sub_appid: params[:item][:sub_appid], # 子商户SubAPPID
         subscribe_appid: params[:item][:subscribe_appid], # 微信分配的服务商公众号或 APP 账号 ID；如为空，则值传NULL（字母大写小写均可）
-      }
+      }.map {|k,v| js[k] = v if v.present? }
       js[:sign] = get_mac js, key
       xml = js.to_xml(root: 'xml', skip_instruct: true, dasherize: false)
       @request = xml
