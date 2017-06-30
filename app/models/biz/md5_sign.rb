c45209c57c92f51122ecf181f15877d62f3daf9e
dalezhang <17652709@qq.com>
2017-06-30 19:03:53 +0800

    123

--- app/models/biz/md5_sign.rb
+++ app/models/biz/md5_sign.rb
@@ -1,19 +1,11 @@
 class Biz::Md5Sign
   def self.get_mab(js)
     mab = []
-    js.deep_symbolize_keys
-    sign_js = {}
-    js.each do |k, v|
-      if v.present?
-        sign_js[k] = v
-      end
-    end
-    sign_js.keys.sort_by {|x| x.downcase}.each do |k|
-      mab << "#{k}=#{js[k].to_s}" if ![:mac, :sign, :controller, :action ].include?(k.to_sym) && js[k].present? && js[k].class != Hash
+    js.keys.sort.each do |k|
+      mab << "#{k}=#{js[k].to_s}" if ![:mac, :sign, :controller, :action ].include?(k.to_sym) && js[k] && js[k].class != Hash
     end
     mab.join('&')
   end
-
   def self.md5(str)
     Digest::MD5.hexdigest(str)
   end
