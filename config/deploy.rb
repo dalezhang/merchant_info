dd1f2c500edac7754c6f4bb76a9850fc0165d29c
DaleZhang <17652709@qq.com>
2017-07-15 19:23:21 +0800

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -89,7 +89,7 @@ task :deploy do
   # run(:local){ say 'done' }
 end
 task :clean_shared_files do
-  command %{rm config/database.yml config/puma.rb config/secrets.yml}
+  command %{rm config/database.yml config/puma.rb }
 end
 task :eye do
   command %{ pwd }
