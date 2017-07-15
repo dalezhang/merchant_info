963ff9dd9175bb08dee7dfa950555f9219b4cb5f
DaleZhang <17652709@qq.com>
2017-07-15 19:12:53 +0800

    delete mongoid.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :branch, 'deploy'
 
 # shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 # set :shared_dirs, fetch(:shared_dirs, []).push('somedir')
-set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml', 'config/puma.rb')
+set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/mongoid.yml','config/secrets.yml', 'config/puma.rb')
 
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
